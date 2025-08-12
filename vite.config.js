a56cbf7f04773dc0cbd620e6f3f628497a3a7f21
Jakub Lipiński <j.lipinski017@gmail.com>
2025-08-12 21:04:05 +0200

    Change Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from "path";
 export default defineConfig({
     plugins: [
         laravel({
-            input: ["resources/js/app.js"],
+            input: ["resources/js/app.js", "resources/sass/style.scss"],
             refresh: true,
         }),
         vue(),
