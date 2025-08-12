cda0c5ec47d04033c251af3d918d8baf8a845526
Jakub Lipiński <j.lipinski017@gmail.com>
2025-08-12 15:25:05 +0200

    Fix styles import

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,6 @@
 import "./bootstrap";
 import { createApp, h } from "vue";
 import { createInertiaApp } from "@inertiajs/vue3";
-import "@/sass/style.scss";
 
 createInertiaApp({
     resolve: (name) => {
